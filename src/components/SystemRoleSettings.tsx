6ab8465efc3ca253acc71a285f9a048d8b269507
huange1995 <755685472@qq.com>
2023-04-21 11:37:39 +0800

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -20,7 +20,7 @@ export default (props: Props) => {
   }
 
   return (
-    <div class="my-4">
+    <div class="my-4" display="none">
       <Show when={!props.systemRoleEditing()}>
         <Show when={props.currentSystemRoleSettings()}>
           <div>
